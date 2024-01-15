d4a5b953f322dafb13a5964c1de93fce4dfedd61
Unai Muñoz <152631520+UnaiMunoz@users.noreply.github.com>
2024-01-15 12:30:04 +0100

    Update game_prueba.py

--- M3/game_prueba.py
+++ M3/game_prueba.py
@@ -1,6 +1,6 @@
 import os
 
-def limpiar_pantalla():
+def clearScreen():
     sistema_operativo = os.name
     if sistema_operativo == 'posix':
         os.system('clear')  # Comando para limpiar la pantalla en sistemas Unix
@@ -23,46 +23,45 @@ def showPrompt():
     else:
         print("There are no actions yet")
 
-def encontrar_personaje(mapa, simbolo):
-    for i in range(len(mapa)):
-        for j in range(len(mapa[i])):
-            if mapa[i][j] == simbolo:
+def find_link(map, symbol):
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == symbol:
                 return i, j
 
-def imprimir_mapa(mapa):
-    for fila in mapa:
-        print(' '.join(fila))
+def print_map(map):
+    for x in map:
+        print(' '.join(x))
     print()
 
-def mover_personaje(mapa, posicion, direccion):
-    nueva_posicion = list(posicion)
+def mover_personaje(map, position, direccion):
+    new_position = list(position)
 
-    if direccion == 'w' and nueva_posicion[0] > 0:
-        nueva_posicion[0] -= 1
-    elif direccion == 's' and nueva_posicion[0] < len(mapa) - 1:
-        nueva_posicion[0] += 1
-    elif direccion == 'a' and nueva_posicion[1] > 0:
-        nueva_posicion[1] -= 1
-    elif direccion == 'd' and nueva_posicion[1] < len(mapa[0]) - 1:
-        nueva_posicion[1] += 1
+    if direccion == 'w' and new_position[0] > 0:
+        new_position[0] -= 1
+    elif direccion == 's' and new_position[0] < len(map) - 1:
+        new_position[0] += 1
+    elif direccion == 'a' and new_position[1] > 0:
+        new_position[1] -= 1
+    elif direccion == 'd' and new_position[1] < len(map[0]) - 1:
+        new_position[1] += 1
     else:
         print("No puedes moverte en esa dirección.")
         return False
 
-    if 0 <= nueva_posicion[0] < len(mapa) and 0 <= nueva_posicion[1] < len(mapa[0]):
-        if mapa[nueva_posicion[0]][nueva_posicion[1]] not in ['#', 'O', 'T', 'C', 'E', 'S', 'M', 'F', '1', '9', '0', '~']:
-            mapa[posicion[0]][posicion[1]] = ' '
-            mapa[nueva_posicion[0]][nueva_posicion[1]] = 'X'
+    if 0 <= new_position[0] < len(map) and 0 <= new_position[1] < len(map[0]):
+        if map[new_position[0]][new_position[1]] not in ['#', 'O', 'T', 'C', 'E', 'S', 'M', 'F', '1', '9', '0', '~']:
+            map[position[0]][position[1]] = ' '
+            map[new_position[0]][new_position[1]] = 'X'
             return True
 
-    print("No puedes moverte en esa dirección o has llegado al límite del mapa.")
     return False
 
 
-def obtener_posicion_personaje(mapa, simbolo):
-    return encontrar_personaje(mapa, simbolo)
+def obtener_position_personaje(map, symbol):
+    return find_link(map, symbol)
 
-mapa = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'O', 'O', 'O'],
+map =  [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'O', 'O', 'O'],
         [' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', 'O', 'O', '~', 'O', 'O', 'O', 'O', '~'],
         [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '~', '~', '~', '~', '~',' ~', ' ', ' ', ' ', '~', '~', '~', '~', '~', '~'],
         [' ', ' ', ' ', ' ', ' ', 'T', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '~', '~', '~'],
@@ -74,28 +73,26 @@ mapa = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '
         ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' ']
 ]
 
-posicion_personaje = encontrar_personaje(mapa, 'X')
-
 while True:
-    limpiar_pantalla()
-    imprimir_mapa(mapa)
+    clearScreen()
+    print_map(map)
     showPrompt()
-    direccion = input("\nIngresa la dirección (W,A,S,D): ")
+    direction = input("\nWhere you want to go? (W,A,S,D): ")
 
-    if direccion.lower() == 'salir':
+    if direction.lower() == 'salir':
         break
 
-    if mover_personaje(mapa, posicion_personaje, direccion.lower()):
-        posicion_personaje = encontrar_personaje(mapa, 'X')
-        if direccion.lower() == "w":
-            addText("Te has movido hacia arriba")
-        elif direccion.lower() == "a":
-            addText("Te has movido hacia la izquierda")
-        elif direccion.lower() == "s":
-            addText("Te has movido hacia abajo")
-        elif direccion.lower() == "d":
-            addText("Te has movido hacia la derecha")
+    if mover_personaje(map, character_position, direction.lower()):
+        character_position = find_link(map, 'X')
+        if direction.lower() == "w":
+            addText("You have moved up")
+        elif direction.lower() == "a":
+            addText("You have moved left")
+        elif direction.lower() == "s":
+            addText("You have moved down")
+        elif direction.lower() == "d":
+            addText("You have moved right")
     else:
-        addText("No puedes moverte en esa dirección o has llegado al límite del mapa.")
+        addText("You cannot move in that direction or you have reached the limit of the map.")
 
-print("Juego terminado.")
+print("Game finished.")
